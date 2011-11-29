2b7d6ebc1b8b443f15e2be4b2ccc85a1f16b5a70
Neeraj Singh <neerajdotname@gmail.com>
2011-11-29 12:46:19 -0500

    secret

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -2,7 +2,9 @@ class PaymentNotificationsController < ApplicationController
 
   protect_from_forgery :except => [:create]
 
+  #
   #curl -d "txn_id=3XC103945N720211C&invoice=923204115&payment_status=Completed" http://localhost:3000/payment_notifications/paypal
+  #
   def paypal
     PaymentNotification.create!(params: params,
                                 payment_provider: 'paypal',
@@ -12,7 +14,9 @@ class PaymentNotificationsController < ApplicationController
     render :nothing => true
   end
 
-  #curl -d "txn_id=3XC103945N720211C&invoice=923204115&payment_status=Completed" http://localhost:3000/payment_notifications/splitable
+  #
+  # curl -d "invoice=923204115&payment_status=paid" http://localhost:3000/payment_notifications/splitable
+  #
   def splitable
     PaymentNotification.create!(params: params,
                                 payment_provider: 'splitable',
