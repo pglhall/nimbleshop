32856c8abd6fea77639f8ea597fe6e9709db90fc
Neeraj Singh <neerajdotname@gmail.com>
2011-11-30 14:35:22 -0500

    key is api_secret

--- lib/splitable.rb
+++ lib/splitable.rb
@@ -9,8 +9,7 @@ class Splitable
     data.merge!(total_amount: 20000)
     data.merge!(invoice: order.id)
     data.merge!(number_of_participants: 20)
-    data.merge!(secret: 'sdfs9smdsddf')
-    data.merge!(notify_url: 'http://localhost:3010/payment_notifications/splitable')
+    data.merge!(api_secret: 'sdfs9smdsddf')
     data.merge!(details_url: 'https://www.splitable.com/sunlife_stadium')
 
     data.merge!(logo_url: 'https://s3.amazonaws.com/splitable-production1/app/public/system/logos/39/regular/sunlifestadium.gif')
