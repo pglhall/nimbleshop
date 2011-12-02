62808c228a04f8439ce52b9be991a674b916b97b
Neeraj Singh <neerajdotname@gmail.com>
2011-12-02 15:54:46 -0500

    add expires_in

--- lib/splitable.rb
+++ lib/splitable.rb
@@ -14,6 +14,7 @@ class Splitable
 
     data.merge!(logo_url: 'https://s3.amazonaws.com/splitable-production1/app/public/system/logos/39/regular/sunlifestadium.gif')
     data.merge!(item_image_url: 'https://s3.amazonaws.com/splitable-production1/app/public/system/images/126/regular/black-eyed-peas.jpg')
+    data.merge!(expires_in: 24)
 
     submission_url + data.to_query
   end
