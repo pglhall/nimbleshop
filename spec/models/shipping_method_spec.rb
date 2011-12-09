1391fd2a66fc9e83224b527c1b5844fd1261ce67
Neeraj Singh <neerajdotname@gmail.com>
2011-12-09 14:30:17 -0500

    tests

--- spec/models/shipping_method_spec.rb
+++ spec/models/shipping_method_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe ShippingMethod do
-  describe "#condition_in_english" do
-    let(:shipping_method)  { create(:shipping_method, shipping_price: 100, lower_price_limit: 1, upper_price_limit: 99)  }
+  describe "#available_for" do
+    let(:shipping_method)  { create(:shipping_method, shipping_price: 100,
+                                                      lower_price_limit: 1,
+                                                      upper_price_limit: 99)  }
+    let(:order)  { create(:order) }
+    before do
+      order.line_items << create(:line_item)
+    end
     it '' do
-      #shipping_method.condition_in_english.must_equal '$1.00 - $99.00'
+      shipping_method.available_for(order).must_equal true
     end
   end
 end
