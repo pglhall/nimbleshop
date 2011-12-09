150ed8115429cec9fe84d6e2f31e30bbfe4e1c21
Neeraj Singh <neerajdotname@gmail.com>
2011-12-09 14:30:10 -0500

    available_order

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -6,4 +6,13 @@ class ShippingMethod < ActiveRecord::Base
   validates :shipping_price,    presence: true
   validates :name,              presence: true
 
+  # indicates if the shipping method is available for the given order
+  def available_for(order)
+    if upper_price_limit
+      (order.amount >= lower_price_limit) && (order.amount <= upper_price_limit)
+    else
+      order.amount >= lower_price_limit
+    end
+  end
+
 end
