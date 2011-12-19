fc991862efca9173843c888b77482db581085e82
Neeraj Singh <neerajdotname@gmail.com>
2011-12-18 22:22:20 -0500

    footer

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,4 +4,7 @@ class Shop < ActiveRecord::Base
   # make sure that twitter input is accepted with both @ and without @. However store the data without @
   # ensure that facebook url starts with http://www or https://www
 
+  def twitter_url
+    twitter_handle.blank? ? nil : "http://twitter.com/#{twitter_handle}"
+  end
 end
