0be37d9200f0813759022341942f5aca2782bf94
Neeraj Singh <neerajdotname@gmail.com>
2011-12-19 05:11:25 -0500

    done

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,7 @@
 class Shop < ActiveRecord::Base
 
+  before_validation :sanitize_twitter_handle
+
   # TODO
   # make sure that twitter input is accepted with both @ and without @. However store the data without @
   # ensure that facebook url starts with http://www or https://www
@@ -7,4 +9,11 @@ class Shop < ActiveRecord::Base
   def twitter_url
     twitter_handle.blank? ? nil : "http://twitter.com/#{twitter_handle}"
   end
+
+  private
+
+  def sanitize_twitter_handle
+    self.twitter_handle.gsub!(/^@/, '')
+  end
+
 end
