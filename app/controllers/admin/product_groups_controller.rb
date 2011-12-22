29bc6294892f91c6b599203da0005d78a24dfdf1
Neeraj Singh <neerajdotname@gmail.com>
2011-12-22 14:56:42 -0500

    fix product group controller

--- app/controllers/admin/product_groups_controller.rb
+++ app/controllers/admin/product_groups_controller.rb
@@ -1,27 +1,27 @@
 class Admin::ProductGroupsController < AdminController
 
+  before_filter :load_product_group, only: [:edit, :update]
+
   def index
     @product_groups = ProductGroup.all
   end
 
-  def show
-    @product_group = ProductGroup.find(params[:id])
-    @products = Product.all
-  end
-
   def edit
-    @product_group = ProductGroup.find(params[:id])
     @custom_fields = CustomField.order('name asc').all
   end
 
   def update
-    @product_group = ProductGroup.find(params[:id])
     if @product_group.update_attributes(params[:product_group])
       redirect_to admin_product_groups_path, notice: 'successfully updated'
     else
       render 'edit'
     end
+  end
 
+  private
+
+  def load_product_group
+    @product_group = ProductGroup.find_by_permalink!(params[:id])
   end
 
 end
