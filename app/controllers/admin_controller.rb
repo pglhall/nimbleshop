063662c37a277c1392b38fdd0e269fa82d594d42
Neeraj Singh <neerajdotname@gmail.com>
2012-01-03 14:05:19 -0500

    added basic http authentication for admin site

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,16 @@
 class AdminController < ApplicationController
   layout 'admin'
 
+  before_filter :authenticate
   before_filter :set_timezone
 
   private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == Settings.http_basic_authentication_password
+    end
+  end
 
   def set_timezone
     Time.zone = @shop.time_zone
