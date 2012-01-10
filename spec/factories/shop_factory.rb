8bba4bffb592d162cf09d63ea4129513cfa5a2b4
Neeraj Singh <neerajdotname@gmail.com>
2012-01-10 15:39:19 -0500

    fixing tests

--- spec/factories/shop_factory.rb
+++ spec/factories/shop_factory.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :shop do |f|
     sequence(:name){|n| "store-#{n}"}
     twitter_handle '@nimbleshop'
+    intercept_email 'johnnie.walker@nimbleshop.com'
+    from_email 'support@nimbleshop.com'
   end
 end
 
