7fe63a75851eb9afc0eb8ab12a7decbabe39da46
Neeraj Singh <neerajdotname@gmail.com>
2012-01-21 21:21:18 -0500

    more tests

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,6 +1,5 @@
 class Variant < ActiveRecord::Base
 
-
   belongs_to :product
 
   validates_presence_of :price
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 describe LineItem do
   describe 'variant present' do
-    let(:product) { create(:product, variants_enabled: true) }
+    let(:product) { create(:product, variants_enabled: true, price: 11) }
     let(:order) { create(:order) }
-    let(:variant) { create(:variant, product: product) }
+    let(:variant) { create(:variant, product: product, price: 27) }
     before do
-      order.add(product, record)
-
+      order.add(product, variant)
+      @line_item = order.line_items.first
+    end
+    it 'should have the price of variant' do
+      order.price.must_equal 27
+      @line_item.price.must_equal 27
+      @line_item.product_name.must_equal product.name
+      @line_item.product_description.must_equal product.description
+      @line_item.variant_info.must_equal "v1, v2, and v3"
     end
   end
 end
