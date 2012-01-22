0e9eb5217fa250e72c4f49ce051f9b327d9d89cb
Neeraj Singh <neerajdotname@gmail.com>
2012-01-21 22:34:19 -0500

    test refactoring

--- spec/models/gateway_processor_spec.rb
+++ spec/models/gateway_processor_spec.rb
@@ -2,62 +2,59 @@ require 'spec_helper'
 
 describe GatewayProcessor do
 
-  describe 'payment stuff' do
+  let(:shop) { create(:shop) }
+  let(:creditcard) { build(:creditcard) }
+  let(:order) { create(:order) }
+  create_authorizenet_payment_method
+  let(:payment_method_permalink) { PaymentMethod.find_by_permalink!('authorize-net').permalink }
+
+  describe '#authorize' do
     before do
-      shop = Factory(:shop)
-      @creditcard = Factory.build(:creditcard)
-      @order = Factory(:order)
-      create_authorizenet_payment_method
-      @payment_method_permalink = PaymentMethod.find_by_permalink!('authorize-net').permalink
-    end
-
-    describe '#authorize' do
-      it '' do
-      GatewayProcessor.new(payment_method_permalink: @payment_method_permalink,
-                           order: @order,
+      GatewayProcessor.new(payment_method_permalink: payment_method_permalink,
+                           order: order,
                            amount: 1234,
-                           creditcard: @creditcard).authorize
-
-        tran = CreditcardTransaction.first
-        CreditcardTransaction.count.must_equal 1
-        tran.status.must_equal 'authorized'
-        tran.active.must_equal true
-        tran.transaction_gid.must_equal '1234567890'
-      end
+                           creditcard: creditcard).authorize
+      @tran = CreditcardTransaction.first
     end
+    it 'should have right values' do
+      @tran.status.must_equal 'authorized'
+      @tran.active.must_equal true
+      @tran.transaction_gid.must_equal '1234567890'
+    end
+  end
 
-    describe '#purchase' do
-      it '' do
-      GatewayProcessor.new(payment_method_permalink: @payment_method_permalink,
-                           order: @order,
+  describe '#purchase' do
+    before do
+      GatewayProcessor.new(payment_method_permalink: payment_method_permalink,
+                           order: order,
                            amount: 1234,
-                           creditcard: @creditcard).purchase
-
-        tran = CreditcardTransaction.first
-        CreditcardTransaction.count.must_equal 1
-        tran.status.must_equal 'purchased'
-        tran.active.must_equal true
-        tran.transaction_gid.must_equal '1234567892'
-      end
+                           creditcard: creditcard).purchase
+      @tran = CreditcardTransaction.first
     end
-
-    describe '#capture' do
-      it '' do
-        GatewayProcessor.new(payment_method_permalink: @payment_method_permalink,
-                             order: @order,
-                             amount: 1234,
-                             creditcard: @creditcard).authorize
-        transaction = CreditcardTransaction.last
-        transaction.capture(payment_method_permalink: @payment_method_permalink)
-
-        tran = CreditcardTransaction.last
-        CreditcardTransaction.count.must_equal 2
-        tran.status.must_equal 'captured'
-        tran.active.must_equal true
-        tran.transaction_gid.must_equal '1234567891'
-      end
+    it 'should have right values' do
+      CreditcardTransaction.count.must_equal 1
+      @tran.status.must_equal 'purchased'
+      @tran.active.must_equal true
+      @tran.transaction_gid.must_equal '1234567892'
     end
+  end
 
+  describe '#capture' do
+    before do
+      GatewayProcessor.new(payment_method_permalink: payment_method_permalink,
+                           order: order,
+                           amount: 1234,
+                           creditcard: creditcard).authorize
+      transaction = CreditcardTransaction.last
+      transaction.capture(payment_method_permalink: payment_method_permalink)
+    end
+    it 'should have right values' do
+      CreditcardTransaction.count.must_equal 2
+      transaction = CreditcardTransaction.last
+      transaction.status.must_equal 'captured'
+      transaction.active.must_equal true
+      transaction.transaction_gid.must_equal '1234567891'
+    end
   end
 
 end
