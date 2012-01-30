4a8701a0061ff5066e77056d70605240048b7f3d
Neeraj Singh <neerajdotname@gmail.com>
2012-01-30 14:10:07 -0500

    refactor test

--- spec/acceptance/admin/products_controller_spec.rb
+++ spec/acceptance/admin/products_controller_spec.rb
@@ -6,63 +6,60 @@ describe "admin integration" do
     it "should be at product show page" do
       visit new_admin_product_path
       page.has_content?("Add new product").must_equal true
-      #save_and_open_page
     end
   end
 
-  describe "add product" do
+  describe "create product" do
+    describe "happy path" do
+      it "should create a new product" do
+        visit admin_products_path
+        click_link '+ Add new product'
 
-    it "should create a product and display it on the list for correct data" do
+        fill_in 'Name', :with => 'the very wicked name for product'
+        fill_in 'Description', :with => 'test desc for user'
+        fill_in 'Price', :with => '45.99'
+        attach_file "Picture", "#{Rails.root}/app/assets/images/visa.png"
+        click_button 'Submit'
 
-      visit admin_products_path
-      click_link '+ Add new product'
-
-      fill_in 'Name', :with => 'the very wicked name for product'
-      fill_in 'Description', :with => 'test desc for user'
-      fill_in 'Price', :with => '45.99'
-      attach_file "Picture", "#{Rails.root}/app/assets/images/visa.png"
-      click_button 'Submit' 
-
-      page.has_content?('Successfuly added').must_equal true
-      page.has_content?('the very wicked name for product').must_equal true
-      page.has_xpath?("//img[@alt='Thumb_visa']").must_equal true
+        page.has_content?('Successfuly added').must_equal true
+        page.has_content?('the very wicked name for product').must_equal true
+        page.has_xpath?("//img[@alt='Thumb_visa']").must_equal true
+      end
     end
 
-    before do
-      Product.destroy_all
+    describe "error path" do
+      it "should not create a product for wrong params" do
+        visit admin_products_path
+        click_link '+ Add new product'
+        attach_file "Picture", "#{Rails.root}/app/assets/stylesheets/admin.css"
+        click_button 'Submit'
+
+        page.has_content?("Name can't be blank").must_equal true
+        page.has_content?("Description can't be blank").must_equal true
+        page.has_content?("Price is not a number").must_equal true
+        page.has_content?("Price can't be blank").must_equal true
+
+        msg = %Q{Pictures picture You are not allowed to upload "css" files, allowed types: ["jpg", "jpeg", "gif", "png"]}
+        page.has_content?(msg).must_equal true
+
+        fill_in 'Price', :with => 'wrong price'
+        click_button 'Submit'
+
+        page.has_content?("Price is not a number").must_equal true
+        page.has_content?("Price can't be blank").must_equal false
+      end
     end
 
-    it "should not create a product for wrong params" do
-      visit admin_products_path
-      click_link '+ Add new product'
-      attach_file "Picture", "#{Rails.root}/app/assets/stylesheets/admin.css"
-      click_button 'Submit'
-
-      page.has_content?("Name can't be blank").must_equal true
-      page.has_content?("Description can't be blank").must_equal true
-      page.has_content?("Price is not a number").must_equal true
-      page.has_content?("Price can't be blank").must_equal true
-      page.has_content?("Pictures picture You are not allowed to upload \"css\" files, allowed types: [\"jpg\", \"jpeg\", \"gif\", \"png\"]").must_equal true
-      
-      fill_in 'Price', :with => 'wrong price'
-      click_button 'Submit'
-
-      page.has_content?("Price is not a number").must_equal true
-      page.has_content?("Price can't be blank").must_equal false
-    end
-        
   end
 
   describe "delete product" do
     before do
-      Product.destroy_all
       create(:product, name: 'the very wicked name for product')
     end
 
     it "should delete a product" do
       visit admin_products_path
       click_link 'Delete'
-      #page.driver.browser.switch_to.alert.accept
       page.has_content?("Successfuly deleted").must_equal true
       page.has_content?('the very wicked name for product').must_equal false
     end
@@ -70,7 +67,6 @@ describe "admin integration" do
 
   describe "edit product" do
     before do
-      Product.destroy_all
       create(:product, name: 'test')
     end
 
@@ -82,7 +78,7 @@ describe "admin integration" do
       fill_in 'Description', :with => 'test desc for user'
       fill_in 'Price', :with => '45.99'
       attach_file "Picture", "#{Rails.root}/app/assets/images/visa.png"
-      click_button 'Submit' 
+      click_button 'Submit'
 
       page.has_content?('Successfuly updated').must_equal true
       page.has_content?('the very wicked name for product').must_equal true
@@ -106,14 +102,10 @@ describe "admin integration" do
     end
 
   end
-  
-  describe "show product" do
-    before do
-      Product.destroy_all
-      create(:product, name: 'test name', description: 'the description', price: 46.99)
-    end
 
+  describe "show product" do
     it "should show product" do
+      create(:product, name: 'test name', description: 'the description', price: 46.99)
       visit admin_products_path
       click_link 'Show'
 
