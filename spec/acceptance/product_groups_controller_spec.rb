20daa695a95e4cb45183a9b176f2972442a001e9
Neeraj Singh <neerajdotname@gmail.com>
2012-01-31 18:43:22 -0500

    change to a bit meaningful test. still largely meaningless

--- spec/acceptance/product_groups_controller_spec.rb
+++ spec/acceptance/product_groups_controller_spec.rb
@@ -2,13 +2,8 @@ require 'spec_helper'
 
 describe "product group integration" do
 
-  before do
-    Shop.destroy_all
-    create(:shop, name: 'Nimble')
-  end
-
   it "should show Nimble shop" do
-    visit '/'
-    page.has_content?('Nimble').must_equal true
+    visit root_path
+    page.has_content?('Shop by category').must_equal true
   end
 end
