ab47cb04136efbada488c3609541655c5d7e6f44
novotarq <novotarq@gmail.com>
2012-02-01 20:11:05 +0100

    payment methods acceptance tests

---
+++ spec/acceptance/admin/payment_methods_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "admin integration" do
+
+  describe "payment methods" do
+    it "should allow to view, enable/disable and edit payment methods" do
+      visit admin_payment_methods_path
+
+      page.has_content?('Setup payment method').must_equal true
+      page.has_link?('Authorize.net').must_equal false
+      page.has_link?('Splitable').must_equal false
+      page.has_link?('Paypal website payments standard').must_equal false
+
+      page.has_checked_field?('paypal-website-payments-standard').must_equal false
+      page.has_checked_field?('authorize-net').must_equal false
+      page.has_checked_field?('splitable').must_equal false
+
+      check 'authorize-net'
+      page.has_content?('Configuration info').must_equal true
+      page.has_content?("56yBAar72").must_equal true
+      click_link 'Edit'
+      fill_in 'Authorize net login', with: '9r3pbDFGDFoihj29f7d'
+      click_button 'Submit'
+
+      page.has_content?('Successfuly updated').must_equal true
+      page.has_content?('9r3pbDFGDFoihj29f7d').must_equal true
+
+      click_link 'Payment methods'
+      page.has_checked_field?('paypal-website-payments-standard').must_equal false
+      page.has_checked_field?('authorize-net').must_equal true
+      page.has_checked_field?('splitable').must_equal false
+
+      page.has_link?('Authorize.net').must_equal true
+      page.has_link?('Splitable').must_equal false
+      page.has_link?('Paypal website payments standard').must_equal false
+
+    end
+  end
+end
