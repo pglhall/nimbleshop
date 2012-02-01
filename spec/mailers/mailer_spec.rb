96984715557d19eaba0c6f1c7281739eed59660a
Neeraj Singh <neerajdotname@gmail.com>
2012-02-01 06:59:19 -0500

    remove meaningless test

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -8,10 +8,5 @@ describe Mailer do
 
   let(:order) { create(:order) }
 
-  describe '#order_notification' do
-    it 'should have one creditcard record if fixtures are read properly' do
-      Creditcard.count.must_equal 1
-    end
-  end
 
 end
