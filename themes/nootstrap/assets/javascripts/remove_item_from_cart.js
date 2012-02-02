51af44d0e55318dbe750736663283f01d04fd1e2
Neeraj Singh <neerajdotname@gmail.com>
2012-02-02 04:07:15 -0500

    use live instead of bind

--- themes/nootstrap/assets/javascripts/remove_item_from_cart.js
+++ themes/nootstrap/assets/javascripts/remove_item_from_cart.js
@@ -1,9 +1,10 @@
-$(function(){
-  $('a.remove-item').bind('click', function(){
-    var $this = $(this),
-        permalink = $this.data('permalink');
-    $('#updates_'+permalink).val(0);
-    $('#cartform').submit();
-    return false;
-  });
+$('a.remove-item').live('click', function(){
+
+  var $this = $(this),
+      permalink = $this.data('permalink');
+
+  $('#updates_'+permalink).val(0);
+  $('#cartform').submit();
+  return false;
+
 });
