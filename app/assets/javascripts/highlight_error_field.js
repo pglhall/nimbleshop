53cd96ebc3ecaa62464a1b083bf2254b4e0cdfdb
Neeraj Singh <neerajdotname@gmail.com>
2012-02-02 04:13:36 -0500

    highlight_error_field is already there in lib

--- app/assets/javascripts/highlight_error_field.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-  $('span.fieldWithErrors').each(function(){
-    var $this = $(this);
-    $this.find('input').addClass('error');
-    $this.closest('.clearfix').addClass('error');
-  });
-
-});
