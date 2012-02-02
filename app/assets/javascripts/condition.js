5bbbbee20c7ed617e8a785f2d95c2325fa3ce577
Neeraj Singh <neerajdotname@gmail.com>
2012-02-02 04:11:10 -0500

    do not make data a global variable

--- app/assets/javascripts/condition.js
+++ app/assets/javascripts/condition.js
@@ -1,6 +1,8 @@
 $(function(){
-	data = $("#product-group-condition-klass").data();
-	$(".condition .field").live("change", function (evt) {
+
+	var data = $("#product-group-condition-klass").data();
+
+  $(".condition .field").live("change", function (evt) {
 		var $target 	= $(evt.target).siblings('input.value').val('').end();
 		var $select 	= $target.siblings('select.operator').html('');
 		var fieldType = $.grep(data.fields, function(element) {
@@ -16,18 +18,22 @@ $(function(){
   $(".add-condition").live("click", function (evt) {
 		var index   = +$(".condition:last").data().index;
 		var params  = $.extend({ index: index+1 }, data);
-		$(".condition:last").after(Mustache.to_html($("#product_template").html(), params));
-		return false;
+
+    $(".condition:last").after(Mustache.to_html($("#product_template").html(), params));
+
+    return false;
 	});
 
   $(".remove-condition").live("click", function (evt) {
 		var $condition = $(evt.target).parents(".condition");
+
     $.each($condition.find(":input:not(:hidden)"), function (i, element) {
 		  $(element).remove();
 		});
 
 		$condition.find(".remove-condition").remove();
 		$condition.find(".destroy-flag").val('1');
+
 		return false;
 	});
 });
