e7b098dfe0fa933103a7b58ee87fd078f40980f5
novotarq <novotarq@gmail.com>
2012-02-03 23:12:44 +0100

    product groups spec - without delete

--- spec/acceptance/admin/product_groups_controller_spec.rb
+++ spec/acceptance/admin/product_groups_controller_spec.rb
@@ -7,26 +7,70 @@ describe "Product Groups integration" do
     create(:date_group_condition)
   end
 
-  describe "index" do
-    it "should be at index page" do
+  describe "good path" do
+    it "should allow to list and create a product group" do
       visit admin_product_groups_path
       page.has_content?("Product groups").must_equal true
       click_button '+ Add new product group'
-p page.body
-      fill_in 'Name', with: 'price < 43'
-      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_0_name']/option['price']").text, :from => 'product_group_product_group_conditions_attributes_0_name')
-      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_0_operator']/option['equal']").text, :from => 'product_group_product_group_conditions_attributes_0_operator')
-      fill_in 'product_group_product_group_conditions_attributes_0_value', with: '43'
+
+      fill_in 'Name', with: 'sweet candies'
+      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_0_name']/option[@value='name']").text, :from => 'product_group_product_group_conditions_attributes_0_name')
+      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_0_operator']/option[@value='contains']").text, :from => 'product_group_product_group_conditions_attributes_0_operator')
+      fill_in 'product_group_product_group_conditions_attributes_0_value', with: 'candy'
       click_link 'add'
-      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_1_name']/option['name']").text, :from => 'product_group_product_group_conditions_attributes_0_name')
-      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_1_operator']/option['contains']").text, :from => 'product_group_product_group_conditions_attributes_0_operator')
-      fill_in 'product_group_product_group_conditions_attributes_1_value', with: 'candy'
+
+      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_1_name']/option[@value='name']").text, :from => 'product_group_product_group_conditions_attributes_1_name')
+      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_1_operator']/option[@value='starts']").text, :from => 'product_group_product_group_conditions_attributes_1_operator')
+      fill_in 'product_group_product_group_conditions_attributes_1_value', with: 'sweet'
       click_button 'Submit'
 
       page.has_content?('Successfuly updated').must_equal true
-      page.has_content?("name contains '43' and name contains 'candy'").must_equal true
+      page.has_content?("name contains 'candy' and name starts with 'sweet'").must_equal true
+
+      click_link 'Edit'
+
+      fill_in 'Name', with: 'awesome candies'
+      fill_in 'product_group_product_group_conditions_attributes_0_value', with: 'awesome'
+      select(find(:xpath, "//*[@id='product_group_product_group_conditions_attributes_0_operator']/option[@value='starts']").text, :from => 'product_group_product_group_conditions_attributes_0_operator')
+      click_link 'remove'
+      click_button 'Submit'
+
+      page.has_content?('Successfuly updated').must_equal true
+      page.has_content?("name starts with 'awesome'").must_equal true
+
+      page.evaluate_script('window.confirm = function() { return true; }')
+
+      # handle_js_confirm do
+      #   click_link 'Delete'
+      # end
+      # page.has_content?("name starts with 'awesome'").must_equal false
 
     end
+  end
+
+  describe "bad path" do
+    it "should not allow to create wrong product group" do
+      visit admin_product_groups_path
+      click_button '+ Add new product group'
+      fill_in 'Name', with: ''
+      click_button 'Submit'
+
+      page.has_content?('Successfuly updated').must_equal false
+      page.has_content?("Product group conditions value is invalid").must_equal true
+      page.has_content?("Product group conditions value can't be blank").must_equal true
+      page.has_content?("Name can't be blank").must_equal true
+    end
+  end
+
+  private
+
+  def handle_js_confirm(accept=true)
+    page.execute_script "window.original_confirm_function = window.confirm"
+    page.execute_script "window.confirmMsg = null"
+    page.execute_script "window.confirm = function(msg) { window.confirmMsg = msg; return #{!!accept}; }"
+    yield
+  ensure
+    page.execute_script "window.confirm = window.original_confirm_function"
   end
 
 end
