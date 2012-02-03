ac0db586e48713b01fde3a96bf5517c645c5ef68
Neeraj Singh <neerajdotname@gmail.com>
2012-02-03 11:11:17 -0500

    refactor test

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe Address do
 
-  describe "of country type" do
+  describe "#validations" do
     let(:address) { build(:address) }
 
-    it "#validations" do
+    it {
       address.wont have_valid(:first_name).when(nil)
       address.wont have_valid(:last_name).when(nil)
       address.wont have_valid(:address1).when(nil)
       address.wont have_valid(:state).when(nil)
       address.wont have_valid(:country).when(nil)
       address.wont have_valid(:zipcode).when(nil)
-      end
+    }
   end
 end
