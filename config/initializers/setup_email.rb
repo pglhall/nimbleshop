6291eb4161e1026c57212aa22d0059a8ab70ebc9
Neeraj Singh <neerajdotname@gmail.com>
2012-02-04 18:49:31 -0500

    setup sendgrid info

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,12 +1,25 @@
-if Rails.env.production?
+ActionMailer::Base.delivery_method = :smtp
+
+if Rails.env.production?|| (Rails.env.staging? && Settings.deliver_email_for_real_in_staging)
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: Settings.sendgrid.username,
+    password: Settings.sendgrid.password,
+    domain: Settings.sendgrid.domain,
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
 else
-  ActionMailer::Base.delivery_method = :smtp
+
   ActionMailer::Base.smtp_settings = {
     :user_name => "nimbleshop",
     :password => "7663e1f272637a4b",
     :address => "mailtrap.io",
     :port => 2525,
     :authentication => :plain }
+
 end
 
