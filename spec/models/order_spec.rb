9c8b61fcd6cd6c6ae427c8e14e84ffd405013efe
Neeraj Singh <neerajdotname@gmail.com>
2012-02-07 10:49:14 -0500

    remove skip

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,20 +2,12 @@ require 'spec_helper'
 
 describe Order do
   describe '#available_shipping_methods' do
-    let(:order)  { create(:order)  }
-    let(:shipping_method)  { create(:country_shipping_method,
-                                                      base_price: 100,
-                                                      lower_price_limit: 1,
-                                                      upper_price_limit: 199)  }
-    before do
+    it {
+      order = create(:order)
+      order.add(create(:product))
+      shipping_method = create(:country_shipping_method, base_price: 100, lower_price_limit: 1, upper_price_limit: 99999)
       order.shipping_address = create(:shipping_address)
-      shipping_method
-    end
-    it 'should have right values' do
-      skip 'subba will look into it' do
-        order.available_shipping_methods.size.must_equal 1
-      end
-    end
+      order.available_shipping_methods.size.must_equal 1
+    }
   end
-
 end
