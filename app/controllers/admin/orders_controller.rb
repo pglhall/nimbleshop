f5a680072f0238302ef3e648e6e40973ec544f08
Neeraj Singh <neerajdotname@gmail.com>
2012-02-17 01:45:48 -0500

    page title

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,14 @@
 class Admin::OrdersController < AdminController
 
   def index
+    @page_title = 'Orders'
     @orders = Order.order('id desc')
   end
 
   def show
     @order = Order.find_by_number!(params[:id])
+    @page_title = "Order # #{@order.number}"
+
     if @order.shipments.any?
       @shipment = @order.shipments.first
     else
