ebe69b4ee8c0628ec1e8665388a29dcd462ba8eb
Neeraj Singh <neerajdotname@gmail.com>
2012-02-17 01:07:28 -0500

    fix admin order show page

--- spec/acceptance/admin/orders_controller_spec.rb
+++ spec/acceptance/admin/orders_controller_spec.rb
@@ -1,21 +1,31 @@
 require 'spec_helper'
 
 describe "admin integration" do
+
   describe "order with no extra information" do
-    it {
+    before do
       order = create(:order)
       order.email = nil
       order.shipping_address = nil
       order.shipping_method = nil
       order.save(validate: false)
-
+    end
+    it {
       visit admin_orders_path
-
       page.has_css?('h1.ns-page-title', text: 'Orders')
       click_link Order.first.number
-      save_and_open_page
-
       assert page.has_content?('Payment status abandoned')
     }
   end
+
+  describe "order with line item" do
+    it {
+      order_with_line_items
+      visit admin_orders_path
+      page.has_css?('h1.ns-page-title', text: 'Orders')
+      click_link Order.first.number
+      assert page.has_content?('Payment status abandoned')
+    }
+  end
+
 end
