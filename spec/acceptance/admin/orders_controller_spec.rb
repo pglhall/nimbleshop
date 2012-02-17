11d1619bedfca10d477469840e24e1731d920928
Neeraj Singh <neerajdotname@gmail.com>
2012-02-17 01:22:23 -0500

    do not refer to product directly

--- spec/acceptance/admin/orders_controller_spec.rb
+++ spec/acceptance/admin/orders_controller_spec.rb
@@ -21,6 +21,10 @@ describe "admin integration" do
   describe "order with line item" do
     it {
       order_with_line_items
+      order = Order.first
+      product = Product.find(order.line_items.first.product_id)
+      product.destroy
+
       visit admin_orders_path
       page.has_css?('h1.ns-page-title', text: 'Orders')
       click_link Order.first.number
