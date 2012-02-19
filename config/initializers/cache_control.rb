f432b4f68a8505f094b2a40dbc073639f48b8959
Neeraj Singh <neerajdotname@gmail.com>
2012-02-19 01:08:04 -0500

    done

--- config/initializers/cache_control.rb
+++ config/initializers/cache_control.rb
@@ -1,6 +1,6 @@
 if Settings.using_heroku
   Nimbleshop::Application.config.serve_static_assets = true
 
-  # cache images for 1 hour
-  Nimbleshop::Application.config.static_cache_control = "public, max-age=3600"
+  # cache images
+  Nimbleshop::Application.config.static_cache_control = "public, max-age=36000"
 end
