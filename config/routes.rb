60b13d613efedba4fe6a3d2e67fab82019759cea
Neeraj Singh <neerajdotname@gmail.com>
2012-02-24 15:46:33 -0500

    rearranged routes, removed commented out stuff and removed hashrocket

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,51 @@ Nimbleshop::Application.routes.draw do
 
   themes_for_rails
 
+  resources :payment_processors
+  resources :product_groups
+  resources :pages,     only: :show
+  resource  :checkout, controller: 'checkout',  only: :show
+
+  resources :instant_payment_notifications do
+    collection do
+      post 'paypal'
+      post 'splitable'
+    end
+  end
+
+  resources :products,  only: [:index, :show] do
+    member do
+      get 'all_pictures'
+    end
+  end
+
+  resources :orders,    only: [:edit, :update] do
+    member do
+      get 'paid'
+      get 'edit_shipping_method'
+      put 'update_shipping_method'
+    end
+  end
+
+  resource  :feedback,  only: [:show] do
+    collection do
+      get 'splitable'
+    end
+  end
+
+  resource :cart, only: [:show, :update] do
+    member do
+      post 'add'
+    end
+  end
+
+  get "/admin",           to: "admin/main#index"
+  get "/reset",           to: "admin/main#reset"
+  get "/paypal_return",   to: "paypal_return#handle"
+
+  root :to => "products#index"
+
+  # BEGINNING of Admin section ---------------------------------------------------
   namespace :admin do
     namespace :paymentmethod do
       resource :authorizedotnet
@@ -46,47 +91,6 @@ Nimbleshop::Application.routes.draw do
     resource  :shop, only: [:update, :edit]
   end
 
-  #resources :creditcard_payments
-  resources :payment_processors
-
-  resources :instant_payment_notifications do
-    collection do
-      post 'paypal'
-      post 'splitable'
-    end
-  end
-  resources :product_groups
-  resources :products,  only: [:index, :show] do
-    member do
-      get 'all_pictures'
-    end
-  end
-  resources :pages,     only: [:show]
-  resources :orders,    only: [:edit, :update] do
-    member do
-      get 'paid'
-      get 'edit_shipping_method'
-      put 'update_shipping_method'
-    end
-  end
-  resource  :checkout, :controller => 'checkout',  :only => [:show]
-  resource  :feedback,  only: [:show] do
-    collection do
-      get 'splitable'
-    end
-  end
-
-  resource :cart, only: [:show, :update] do
-    member do
-      post 'add'
-    end
-  end
-
-  get "/admin",    :to => "admin/main#index"
-  get "/reset",    :to => "admin/main#reset"
-
-  get "/paypal_return",    :to => "paypal_return#handle"
-
-  root :to => "products#index"
+  # END of Admin section ---------------------------------------------------
 
 end
