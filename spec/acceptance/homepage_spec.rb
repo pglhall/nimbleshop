09fa54038dfa47a4393b89c6e6a2e71aae3d890e
Neeraj Singh <neerajdotname@gmail.com>
2012-03-02 12:31:11 -0500

    homepage spec

---
+++ spec/acceptance/homepage_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "homepage integration" do
+
+  it "should be ok for good path" do
+    visit root_path
+    page.has_content?('powered by').must_equal true
+
+    LinkGroup.delete_all
+    visit root_path
+    page.has_content?('powered by').must_equal true
+
+  end
+end
