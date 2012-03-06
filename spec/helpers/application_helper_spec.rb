58dfb477cd2c377e8bfac71d676b3b4b0994a3a1
Megpha <megpha@yahoo.com>
2012-03-06 13:46:15 -0500

    fixing build

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
+class DummyClass < ActionView::Base
+  include ApplicationHelper
+end
 
 describe ApplicationHelper do
+  def helper
+    DummyClass.new
+  end
+
   describe "#unconfigured_countries" do
     it "should existing country shipping zones" do
-      result = unconfigured_shipping_zone_countries.map {|_,t| t }
+      result = helper.unconfigured_shipping_zone_countries.map {|_,t| t }
       result.must_include 'HK'
-
       create(:country_shipping_zone, country_code: 'HK')
-      result = unconfigured_shipping_zone_countries.map {|_,t| t }
+      result = helper.unconfigured_shipping_zone_countries.map {|_,t| t }
       result.wont_include 'HK'
     end
   end
