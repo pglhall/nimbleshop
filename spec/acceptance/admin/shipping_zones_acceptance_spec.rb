72422dcf4fd991d1ece321b27e102d1aca93a11d
Neeraj Singh <neerajdotname@gmail.com>
2012-03-30 22:32:30 -0400

    cleaned up acceptance test

--- spec/acceptance/admin/shipping_zones_acceptance_spec.rb
+++ spec/acceptance/admin/shipping_zones_acceptance_spec.rb
@@ -2,21 +2,17 @@ require 'spec_helper'
 
 describe "shipping_zones_acceptance_spec integration" do
 
-  it "should create new shipping zone" do
-    visit "/admin"
-    click_link "Shipping zones"
-    click_link "Add new shipping zone"
-  end
-
   def assert_regions_created(count)
     bef = RegionalShippingZone.count
     yield
     assert_equal count, RegionalShippingZone.count - bef
   end
 
-  describe "should add state level shipping methods when new shipping zone is added" do
+  describe "should add state level shipping methods when new shipping zone is created" do
     it {
-      visit new_admin_shipping_zone_path
+      visit admin_path
+      click_link 'shipping zones'
+      click_link 'add_new_shipping_zone'
       assert_regions_created(13) do
         select 'Canada', :from => 'Country name'
         click_button('Submit')
