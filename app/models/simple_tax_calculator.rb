e0372efe7838911195ed8be5da82069d42e7c952
Megpha <megpha@yahoo.com>
2012-04-07 10:46:34 -0400

    use big decimal for tax calculation

--- app/models/simple_tax_calculator.rb
+++ app/models/simple_tax_calculator.rb
@@ -1,9 +1,15 @@
 class SimpleTaxCalculator
+
   def initialize(order)
     @order = order
   end
 
   def tax
-    @order.price * Shop.first.tax_percentage * 0.01
+    (@order.price * tax_percentage * BigDecimal.new("0.01")).to_f.round(2)
   end
+
+  private
+    def tax_percentage
+      Shop.first.tax_percentage
+    end
 end
