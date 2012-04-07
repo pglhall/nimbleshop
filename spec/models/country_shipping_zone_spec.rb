171f7b2780b4f763d912ebc9b0bdb1f413176e42
Megpha <megpha@yahoo.com>
2012-04-07 10:00:22 -0400

    moving to test/unit

--- spec/models/country_shipping_zone_spec.rb
+++ spec/models/country_shipping_zone_spec.rb
@@ -1,33 +1,15 @@
-require 'spec_helper'
+require 'test_helper'
 
-describe CountryShippingZone do
-
-  describe "assign name from carmen" do
-    it {
-      create(:country_shipping_zone).name.must_equal "United States"
-    }
+class CountryShippingZoneTest < ActiveRecord::TestCase
+  test "assign name from carmen" do
+    assert_equal "United States", create(:country_shipping_zone).name
   end
 
-  describe "#validations" do
-    subject { create(:country_shipping_zone) }
+  test "should create all regions from canada" do
+    regions = Carmen::Country.coded("CA").subregions.length
 
-    it do 
-      wont allow_value("XXX").for(:country_code)
-      must validate_presence_of(:country_code)
-    end
-  end
-
-  describe "#callbacks" do
-    it "should create all regions from canada" do
-      canada  = create(:country_shipping_zone, country_code: 'CA')
-      regions = Carmen::Country.coded("CA").subregions
-      canada.regional_shipping_zones.length.must_equal regions.length
-    end
-
-    it "should create all regions for USA" do
-      usa     = create(:country_shipping_zone, country_code: 'US')
-      regions = Carmen::Country.coded("US").subregions
-      usa.regional_shipping_zones.length.must_equal regions.length
+    assert_difference 'RegionalShippingZone.count', regions do
+      create(:country_shipping_zone, country_code: 'CA')
     end
   end
 end
--- spec/models/regional_shipping_zone_spec.rb
+++ spec/models/regional_shipping_zone_spec.rb
@@ -1,18 +1,10 @@
-require 'spec_helper'
+require 'test_helper'
 
-describe RegionalShippingZone do
-  describe "#validations" do
-    let(:us) { create(:country_shipping_zone, country_code: "US") }
-    subject { us.regional_shipping_zones.find_by_state_code("FL") } 
+class RegionalShippingZoneTest < ActiveRecord::TestCase
+  test "validate presence of name" do
+    zone = RegionalShippingZone.new
 
-    it {
-      must validate_presence_of(:state_code)
-      wont allow_value("BAD").for(:state_code)
-      wont allow_value("AZ").for(:state_code)
-
-      us.regional_shipping_zones.find_by_state_code("AL").delete
-
-      must allow_value("AL").for(:state_code)
-    }
+    assert zone.invalid?
+    assert_equal ["can't be blank"], zone.errors[:state_code]
   end
 end
