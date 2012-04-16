9fdf63b47dbf5c100f587ffd5166993a1df4d1f0
Neeraj Singh <neerajdotname@gmail.com>
2012-04-15 23:06:50 -0400

    more tests refactorings

--- spec/acceptance/admin/link_groups_acceptance_spec.rb
+++ spec/acceptance/admin/link_groups_acceptance_spec.rb
@@ -1,46 +1,40 @@
-require 'spec_helper'
+require "test_helper"
 
-describe "link_groups_acceptance_spec integration" do
-  before do
+
+class LinkGroupsAcceptanceTest < ActionDispatch::IntegrationTest
+
+  setup do
     @product_group = create :product_group, name: "Nike Shoes"
     @link_group    = create :link_group,    name: "Branded Shoes"
   end
 
-  describe "add a new link group" do
-    it {
-      visit admin_path
-      click_link 'Link groups'
-      click_link 'add_new_link_group'
-      fill_in 'link_group_name', with: 'Popular products'
-      click_button 'Submit'
-      assert page.has_content?('Successfuly added')
-      assert page.has_content?('Popular products')
-    }
+  test "add a new link group" do
+    visit admin_path
+    click_link 'Link groups'
+    click_link 'add_new_link_group'
+    fill_in 'link_group_name', with: 'Popular products'
+    click_button 'Submit'
+    assert page.has_content?('Successfuly added')
+    assert page.has_content?('Popular products')
   end
 
-  describe "add new link" do
-    it do
-      visit admin_path
-      click_link 'Link groups'
-      refute page.has_content?('Nike Shoes')
-      click_link 'Add new link'
-      select "Nike Shoes", from: 'Product Group'
-      click_button "Add"
-      assert page.has_content?('Nike Shoes')
-    end
+  test "add new link" do
+    visit admin_path
+    click_link 'Link groups'
+    refute page.has_content?('Nike Shoes')
+    click_link 'Add new link'
+    select "Nike Shoes", from: 'Product Group'
+    click_button "Add"
+    assert page.has_content?('Nike Shoes')
   end
 
   describe "delete link group" do
-    before do
-      @nav = @link_group.navigations.create(product_group: @product_group)
-      Capybara.current_driver =  :selenium
-    end
-
-    it {
-      visit admin_link_groups_path
-      page.evaluate_script('window.confirm = function() { return true; }')
-      find("a[href='#{admin_link_group_navigation_path(link_group_id: @link_group, id: @nav)}']").click
-      assert page.has_content?('Successfully deleted')
-    }
+    @nav = @link_group.navigations.create(product_group: @product_group)
+    Capybara.current_driver =  :selenium
+    visit admin_link_groups_path
+    page.evaluate_script('window.confirm = function() { return true; }')
+    find("a[href='#{admin_link_group_navigation_path(link_group_id: @link_group, id: @nav)}']").click
+    assert page.has_content?('Successfully deleted')
   end
+
 end
--- spec/models/shipping_method_spec.rb
+++ spec/models/shipping_method_spec.rb
@@ -17,124 +17,135 @@ class ShippingMethodTest < ActiveSupport::TestCase
 
 end
 
+class ShippingMethodScopesTest < ActiveSupport::TestCase
+  include ::RegionalShippingMethodTestHelper
+
+  def with_regions(countries)
+    countries + countries.map(&:regions).flatten
+  end
+
+  def assert_data_exists_in_result(data, result)
+    assert (data.map(&:id) - result.map(&:id)).blank?
+  end
+
+  setup do
+    @us = create(:country_shipping_zone, country_code: 'US')
+    @ca = create(:country_shipping_zone, country_code: 'CA')
+    @hk = create(:country_shipping_zone, country_code: 'HK')
+
+    @usground1  = create(:shipping_method, lower_price_limit: 0, higher_price_limit: nil, base_price: 3.99, shipping_zone: @us)
+    @usair1     = create(:shipping_method, lower_price_limit: 0, higher_price_limit: 600, base_price: 8.99, shipping_zone: @us)
+    @hkground1  = create(:shipping_method, lower_price_limit: 100, higher_price_limit: 1000, base_price: 8.99, shipping_zone: @hk)
+    @caground1  = create(:shipping_method, lower_price_limit: 0, higher_price_limit: 700, base_price: 5.99, shipping_zone: @ca)
+    @hkair1     = create(:shipping_method, lower_price_limit: 900, higher_price_limit: 2000, base_price: 28.99, shipping_zone: @hk)
+  end
+
+  test "#in_price_range - knows in which shipping methods are available" do
+    assert_data_exists_in_result with_regions([@hkair1, @usground1]) , ShippingMethod.in_price_range(1500).to_ary
+  end
+
+  test "#atmost - knows who price is less than the given amount" do
+    assert_data_exists_in_result with_regions([ @usground1, @hkground1, @hkair1]), ShippingMethod.atmost(1000).to_ary
+    assert_data_exists_in_result with_regions([ @usground1, @usair1, @hkground1, @caground1, @hkair1]), ShippingMethod.atmost(0).to_ary
+  end
+
+  test "#aleast - knows who price is greater than the given amount" do
+    assert_data_exists_in_result with_regions([ @usground1, @usair1, @caground1]), ShippingMethod.atleast(10).to_ary
+  end
+
+  test "#in_country - knows by country code" do
+    assert_data_exists_in_result with_regions([@usground1, @usair1]) , ShippingMethod.in_country('US').to_ary
+  end
+
+  test "#available_countries" do
+    ShippingMethod.available_for_countries(1000).must_have_same_elements ['US', 'HK']
+  end
+end
+
+class ShippingMethodMiscTest < ActiveSupport::TestCase
+  include RegionalShippingMethodTestHelper
+
+  test "making country level shipping method inactive should make children inactive - updating record" do
+    shipping_method = create(:country_shipping_method)
+    region = shipping_method.regions.first
+    region.active.must_equal true
+    shipping_method.update_attributes!(active: false)
+    region.reload
+    region.active.must_equal false
+  end
+
+  test "making country level shipping method inactive should make children inactive - creating record" do
+    shipping_method = create(:country_shipping_method, active: false)
+    region = shipping_method.regions.first
+    region.active.must_equal false
+  end
+
+  test "#enable! #disable!" do
+    shipping = create(:country_shipping_method)
+
+    shipping.enable!
+    shipping.must_be(:active)
+
+    shipping.disable!
+    shipping.wont_be(:active)
+  end
+
+  test "of country - is not going to update" do
+    shipment = ShippingMethod.new(base_price: 10)
+    shipment.shipping_zone = CountryShippingZone.new
+    shipment.update_offset(0.20)
+    shipment.offset.to_f.must_equal 0.0
+  end
+  
+  test "#update_offset - of state - is going to increase by 0.20" do
+    shipment = ShippingMethod.new(base_price: 10)
+    shipment.shipping_zone = RegionalShippingZone.new
+    shipment.update_offset(0.20)
+    shipment.offset.to_f.must_equal 0.20
+  end
+
+  test "#update_offset - of state - is going to decreate by 0.20" do
+    shipment = ShippingMethod.new(base_price: 10)
+    shipment.shipping_zone = RegionalShippingZone.new
+    shipment.offset = 0.80
+    shipment.update_offset(-0.20)
+    shipment.offset.to_f.must_be_within_delta 0.60
+  end
+
+  test "of country type - #shipping_price - ignores offset value" do
+    shipping = build(:country_shipping_method)
+    shipping.offset = 0.10
+    shipping.base_price = 10
+    shipping.shipping_price.must_equal 10.0
+  end
+
+  test "of country type - on create #callbacks - creates all regional shipping zone records" do
+    shipping = build(:country_shipping_method)
+    shipping.save
+    shipping.regions.count.must_equal 57
+  end
+
+  describe "of state type - #shipping_price - ignores base_price value" do
+    shipping = create_regional_shipping_method
+    shipping.parent.base_price = 10
+    shipping.offset = 0.10
+    shipping.base_price = 20
+
+    shipping.shipping_price.to_f.must_equal 10.10
+  end
+
+  test "of state type - on create #callbacks - does not create all regional shipping zone records" do
+    shipping = create_regional_shipping_method
+    shipping.save
+    shipping.regions.count.must_equal 0
+  end
+
+end
 
 require 'spec_helper'
 
 describe ShippingMethod do
   include RegionalShippingMethodTestHelper
-
-  describe "#scopes" do
-    def with_regions(countries)
-      countries + countries.map(&:regions).flatten
-    end
-    before do
-      @us = create(:country_shipping_zone, country_code: 'US')
-      @ca = create(:country_shipping_zone, country_code: 'CA')
-      @hk = create(:country_shipping_zone, country_code: 'HK')
-
-      @usground1  = create(:shipping_method, lower_price_limit: 0, higher_price_limit: nil, base_price: 3.99, shipping_zone: @us)
-      @usair1     = create(:shipping_method, lower_price_limit: 0, higher_price_limit: 600, base_price: 8.99, shipping_zone: @us)
-      @hkground1  = create(:shipping_method, lower_price_limit: 100, higher_price_limit: 1000, base_price: 8.99, shipping_zone: @hk)
-      @caground1  = create(:shipping_method, lower_price_limit: 0, higher_price_limit: 700, base_price: 5.99, shipping_zone: @ca)
-      @hkair1     = create(:shipping_method, lower_price_limit: 900, higher_price_limit: 2000, base_price: 28.99, shipping_zone: @hk)
-    end
-
-    describe "#in_price_range" do
-      it "knows in which shipping methods are available" do
-        result = ShippingMethod.in_price_range(1500).to_ary
-        result.must_have_same_elements with_regions([@hkair1, @usground1])
-      end
-    end
-
-    describe "#atmost" do
-      it "knows who price is less than the given amount" do
-        ShippingMethod.atmost(1000).to_ary.must_have_same_elements  with_regions([ @usground1, @hkground1, @hkair1])
-        ShippingMethod.atmost(0).to_ary.must_have_same_elements with_regions([ @usground1, @usair1, @hkground1, @caground1, @hkair1])
-      end
-    end
-
-    describe "#aleast" do
-      it "knows who price is greater than the given amount" do
-        ShippingMethod.atleast(10).to_ary.must_have_same_elements  with_regions([ @usground1, @usair1, @caground1])
-      end
-    end
-
-    describe "#in_country" do
-      it "knows by country code" do
-        ShippingMethod.in_country('US').to_ary.must_have_same_elements with_regions([@usground1, @usair1])
-      end
-    end
-
-    describe "#available_countries" do
-      it "knows countries where shipping method available for the price" do
-        ShippingMethod.available_for_countries(1000).must_have_same_elements ['US', 'HK']
-      end
-    end
-  end
-
-  describe "making country level shipping method inactive should make children inactive" do
-    describe "updating record" do
-      it {
-        shipping_method = create(:country_shipping_method)
-        region = shipping_method.regions.first
-        region.active.must_equal true
-        shipping_method.update_attributes!(active: false)
-        region.reload
-        region.active.must_equal false
-      }
-    end
-    describe "creating record" do
-      it {
-        shipping_method = create(:country_shipping_method, active: false)
-        region = shipping_method.regions.first
-        region.active.must_equal false
-      }
-    end
-  end
-
-  describe "#enable! #disable!" do
-    it {
-      shipping = create(:country_shipping_method)
-
-      shipping.enable!
-      shipping.must_be(:active)
-
-      shipping.disable!
-      shipping.wont_be(:active)
-    }
-  end
-
-  describe "#update_offset" do
-    let(:shipment) { ShippingMethod.new(base_price: 10) }
-    describe "of country" do
-      it "is not going to update" do
-        shipment.shipping_zone = CountryShippingZone.new
-
-        shipment.update_offset(0.20)
-
-        shipment.offset.to_f.must_equal 0.0
-      end
-    end
-
-    describe "of state" do
-      it "is going to increase by 0.20" do
-        shipment.shipping_zone = RegionalShippingZone.new
-
-        shipment.update_offset(0.20)
-
-        shipment.offset.to_f.must_equal 0.20
-      end
-
-      it "is going to decreate by 0.20" do
-        shipment.shipping_zone = RegionalShippingZone.new
-        shipment.offset = 0.80
-
-        shipment.update_offset(-0.20)
-
-        shipment.offset.to_f.must_be_within_delta 0.60
-      end
-    end
-  end
 
   describe "#available_for" do
     describe "with one shipping method" do
@@ -181,44 +192,4 @@ describe ShippingMethod do
     end
   end
 
-  describe "of country type" do
-    let(:shipping) { build(:country_shipping_method) }
-
-    describe "#shipping_price" do
-      it "ignores offset value" do
-        shipping.offset = 0.10
-        shipping.base_price = 10
-
-        shipping.shipping_price.must_equal 10.0
-      end
-    end
-
-    describe "on create #callbacks" do
-      it "creates all regional shipping zone records" do
-        shipping.save
-        shipping.regions.count.must_equal 57
-      end
-    end
-  end
-
-  describe "of state type" do
-    let(:shipping) { create_regional_shipping_method }
-
-    describe "#shipping_price" do
-      it "ignores base_price value" do
-        shipping.parent.base_price = 10
-        shipping.offset = 0.10
-        shipping.base_price = 20
-
-        shipping.shipping_price.to_f.must_equal 10.10
-      end
-    end
-
-    describe "on create #callbacks" do
-      it "does not create all regional shipping zone records" do
-        shipping.save
-        shipping.regions.count.must_equal 0
-      end
-    end
-  end
 end
