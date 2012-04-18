27799200577beb7b12798b96fcca3e9914540114
Neeraj Singh <neerajdotname@gmail.com>
2012-04-18 19:08:53 -0400

    Revert "using latest themes_for_rails"
    This reverts commit fe1f60c8c7f9f8f2a9b60f5f5afa3929b7d291be.

--- config/initializers/001_settings_application.rb
+++ config/initializers/001_settings_application.rb
@@ -3,7 +3,8 @@ require "#{Rails.root}/lib/config_loader"
 
 Settings = ConfigLoader.new('application.yml').load
 
-# this is neeed to make attach_picture work
-class FilelessIO < StringIO
-  attr_accessor :original_filename
-end
+  # this is neeed to make attach_picture work
+  class FilelessIO < StringIO
+    attr_accessor :original_filename
+  end
+
