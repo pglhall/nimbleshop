c8a1d94faf7be0714886c4b661bb715873834eb7
Megpha <megpha@yahoo.com>
2012-04-18 22:47:21 -0400

    fixing paypal settings update

--- app/controllers/admin/paymentmethod/paypalwebsite_payments_standards_controller.rb
+++ app/controllers/admin/paymentmethod/paypalwebsite_payments_standards_controller.rb
@@ -11,12 +11,7 @@ class Admin::Paymentmethod::PaypalwebsitePaymentsStandardsController < Admin::Pa
   end
 
   def update
-    @payment_method.merchant_email_address = params[:merchant_email_address]
-    @payment_method.return_url             = params[:return_url]
-    @payment_method.notify_url             = params[:notify_url]
-    @payment_method.request_submission_url = params[:request_submission_url]
-
-    @payment_method.save
+    @payment_method.update_attributes(params.slice(:paypal_website_payments_standard_merchant_email, :paypal_website_payments_standard_paymentaction))
     redirect_to admin_paymentmethod_paypalwebsite_payments_standard_path, notice: 'Successfuly updated'
   end
 
@@ -25,5 +20,4 @@ class Admin::Paymentmethod::PaypalwebsitePaymentsStandardsController < Admin::Pa
     def load_payment_method
       @payment_method = PaymentMethod.find_by_permalink!('paypal-website-payments-standard')
     end
-
 end
