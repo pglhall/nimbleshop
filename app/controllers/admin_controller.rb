b7fddc971ba37331d3ebd04e0fad82f62c48a3ff
Neeraj Singh <neerajdotname@gmail.com>
2012-04-21 17:24:39 -0400

    restricted access in production

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,10 +4,19 @@ class AdminController < ApplicationController
 
   before_filter :set_timezone
 
+  before_filter :restricted_access
+
   private
 
     def set_timezone
       Time.zone = current_shop.time_zone
+    end
+
+    def restricted_access
+      return if Rails.env.test? || Rails.env.development?
+      authenticate_or_request_with_http_basic('staging') { |username, password|
+        username == 'admin@example.com' && password == 'welcome'
+      }
     end
 
 end
