fe8a27b1fbe1dcc177f5b5bb6cd372fed7bd74b5
Neeraj Singh <neerajdotname@gmail.com>
2012-04-23 15:18:12 -0400

    initializers clean up

--- config/initializers/cache_images.rb
+++ config/initializers/cache_images.rb
@@ -1,6 +1,4 @@
 if Settings.using_heroku
-  Nimbleshop::Application.config.serve_static_assets = true
-
   # cache images
   Nimbleshop::Application.config.static_cache_control = "public, max-age=36000"
 end
---
+++ config/initializers/serve_static_assets.rb
@@ -0,0 +1,3 @@
+if Settings.using_heroku
+  #Nimbleshop::Application.config.serve_static_assets = true
+end
