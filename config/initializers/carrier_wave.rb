3a898675f9e2195800c07c6fb682eac90ba28e4b
Neeraj Singh <neerajdotname@gmail.com>
2012-05-03 16:32:27 -0400

    if bucket_name is blank then abort with a friendly message

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,20 @@
 if Settings.use_s3
 
+  bucket_name = Settings.s3.bucket_name
+
+  msg = %Q{
+    This application's #{Rails.env} environment is configured to use amazon s3. 
+    However bucket_name to use is not specified.
+    Please check guide( link here) on how to configure application to use S3.
+  }
+
+  raise msg
+
   CarrierWave.configure do |config|
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.storage = :fog
     config.enable_processing = true
-    config.fog_directory  = Settings.s3.bucket_name
+    config.fog_directory  = bucket_name
     config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
     config.fog_public     = true
 
