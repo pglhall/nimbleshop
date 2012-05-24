cb5fe55f0cde7ac1699f168b073063ed0b2de01a
Neeraj Singh <neerajdotname@gmail.com>
2012-05-24 14:24:51 -0400

    cleaned up nootstrap.js file

--- themes/nootstrap/assets/javascripts/nootstrap.js
+++ themes/nootstrap/assets/javascripts/nootstrap.js
@@ -1,15 +1,16 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
+
 //= require jquery-1.7.1
 //= require jquery-ui-1.8.16
 //= require accounting
 //= require twitter/bootstrap
 //= require jquery-ujs
-//
+
+// loaded using fancybox-rails gem
+//= require fancybox
+
+//= require utils/formatNumber
+//= require utils/formatPrice
+
 //= require shipping_address_same_as_billing_address
 //= require thumbnail
 //= require highlight_error_field
@@ -18,8 +19,6 @@
 //= require load_states
 //= require order
 //= require highlight_hovered_picture
-//= require fancybox
+
 //= require carousel
 
-//= require utils/formatNumber
-//= require utils/formatPrice
