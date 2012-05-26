513d819d1c5750f1f2527753e3a5b55fc16e7161
Neeraj Singh <neerajdotname@gmail.com>
2012-05-25 20:06:34 -0400

    some more code refactoring

--- vendor/engines/nimbleshop_authorizedotnet/app/controllers/nimbleshop_authorizedotnet/payments_controller.rb
+++ vendor/engines/nimbleshop_authorizedotnet/app/controllers/nimbleshop_authorizedotnet/payments_controller.rb
@@ -2,30 +2,28 @@ module NimbleshopAuthorizedotnet
   class PaymentsController < ::Admin::PaymentMethodsController
 
     def create
-      order = Order.find_by_id(session[:order_id])
+      order             =  Order.find_by_id(session[:order_id])
       address_attrs     = order.final_billing_address.to_credit_card_attributes
       creditcard_attrs  = params[:creditcard].merge(address_attrs)
-      creditcard       = Creditcard.new(creditcard_attrs)
+      creditcard        = Creditcard.new(creditcard_attrs)
       handler           = NimbleshopAuthorizedotnet::Billing.new(order)
 
       default_action = Shop.first.default_creditcard_action
+
       if handler.send(default_action, creditcard: creditcard)
-        @url = main_app.order_path(order)
+        url = main_app.order_path(order)
+        @output = "window.location='#{url}'"
       else
-        @error = creditcard.errors.full_messages.first
-        Rails.logger.info "Error: #{@error}"
+        error = creditcard.errors.full_messages.first
+        Rails.logger.info "Error: #{error}"
+        @output = "alert('#{@error}')"
       end
 
       respond_to do |format|
         format.js do
-          if @url
-            render js: "window.location='#{@url}'"
-          else 
-            render js: "alert('#{@error}')"
-          end
+          render js: @output
         end
       end
-
 
     end
 
