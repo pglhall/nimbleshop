725aa52f2ebafc5eda65df06321cd891a8c75eb1
Neeraj Singh <neerajdotname@gmail.com>
2012-05-29 15:33:23 -0400

    added test of paying using authorizedotnet

--- test/acceptance/admin/orders_acceptance_test.rb
+++ test/acceptance/admin/orders_acceptance_test.rb
@@ -2,12 +2,24 @@ require "test_helper"
 
 class OrdersAcceptanceTest <  ActionDispatch::IntegrationTest
 
-  setup do
-    order = build :order, email: nil, shipping_address: nil, shipping_method: nil
-    order.save(validate: false)
+  test "paid using authorize.net" do
+    payment_transaction = PaymentTransaction.find_by_id(1)
+    order = create(:order_paid_using_authorizedotnet, payment_status: 'authorized')
+    payment_transaction.update_attribute(:order_id, order.id)
+
+    visit admin_path
+    click_link 'Orders'
+
+    click_link Order.first.number
+    save_and_open_page
+    assert page.has_content?('Authorize.net')
+    assert page.has_content?('Payment status authorized')
   end
 
   test "payment status abandoned" do
+    order = build :order, email: nil, shipping_address: nil, shipping_method: nil
+    order.save(validate: false)
+
     visit admin_path
     click_link 'Orders'
 
@@ -17,6 +29,9 @@ class OrdersAcceptanceTest <  ActionDispatch::IntegrationTest
   end
 
   test "show order with line item and product is deleted" do
+    order = build :order, email: nil, shipping_address: nil, shipping_method: nil
+    order.save(validate: false)
+
     order  =  create :order_with_line_items
     order.line_items.first.product.destroy
 
--- test/factories/order_factory.rb
+++ test/factories/order_factory.rb
@@ -9,6 +9,13 @@ FactoryGirl.define do
       after(:create) { | r | FactoryGirl.create(:line_item, order: r) }
     end
 
+    trait :authorizedotnet do
+      payment_method_id 2
+    end
+
     factory :order_with_line_items,   traits: [:line_items]
+
+    factory :order_paid_using_authorizedotnet,   traits: [:authorizedotnet]
+
   end
 end
