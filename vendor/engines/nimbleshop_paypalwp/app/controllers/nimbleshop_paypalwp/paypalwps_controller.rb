0c2821c7d6132138f9986b9fb041b2a6431fcd1f
Neeraj Singh <neerajdotname@gmail.com>
2012-05-29 23:44:06 -0400

    name has changed from Paypalextension to NimbleshopPaypalwp

--- vendor/engines/nimbleshop_paypalwp/app/controllers/nimbleshop_paypalwp/paypalwps_controller.rb
+++ vendor/engines/nimbleshop_paypalwp/app/controllers/nimbleshop_paypalwp/paypalwps_controller.rb
@@ -1,12 +1,12 @@
 module NimbleshopPaypalwp
   class PaypalwpsController < ::Admin::PaymentMethodsController
 
-    protect_from_forgery except: [ :notify ]
+    protect_from_forgery except: :notify
 
-    before_filter :load_payment_method, except: [ :notify ]
+    before_filter :load_payment_method, except: :notify
 
     def notify
-      handler = PaypalExtension::Billing.new(raw_post: request.raw_post)
+      handler = NimbleshopPaypalwp::Billing.new(raw_post: request.raw_post)
       handler.authorize
       render nothing: true
     end
