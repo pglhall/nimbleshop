ef90e8546cb22a78f8222d86f9c93d56f263a9d6
Neeraj Singh <neerajdotname@gmail.com>
2012-05-31 21:07:07 -0400

    fixing test because of removal of contact_email and intercept_email

--- test/acceptance/admin/shops_acceptance_test.rb
+++ test/acceptance/admin/shops_acceptance_test.rb
@@ -14,8 +14,6 @@ class ShopsAcceptanceTest < ActionDispatch::IntegrationTest
     fill_in 'Theme', with: 'my-awesome-theme'
     fill_in 'Twitter handle', with: '@nimbleshop2'
     fill_in 'Facebook url', with: 'http://www.facebook.com/pages/NimbleSHOP/119319381517845'
-    fill_in 'Contact email', with: 'shop@nimbleshop.com'
-    fill_in 'Intercept email', with: 'shop@nimbleshop.com'
     fill_in 'From email', with: 'shop@nimbleshop.com'
     choose 'Authorize and capture the credit card for the total amount of the order .'
     select(find(:xpath, "//*[@id='shop_time_zone']/option[2]").text, :from => 'shop_time_zone')
@@ -31,21 +29,16 @@ class ShopsAcceptanceTest < ActionDispatch::IntegrationTest
     fill_in 'Theme', with: ''
     fill_in 'Twitter handle', with: ''
     fill_in 'Facebook url', with: ''
-    fill_in 'Contact email', with: ''
-    fill_in 'Intercept email', with: ''
     fill_in 'From email', with: ''
     click_button 'Submit'
 
     assert page.has_content?('From email is invalid')
-    assert page.has_content?('Intercept email is invalid')
     assert page.has_content?("Name can't be blank")
     assert page.has_content?("Theme can't be blank")
 
-    fill_in 'Contact email', with: 'te@'
     fill_in 'Facebook url', with: 'wrong url'
     click_button 'Submit'
 
     assert page.has_content?('Facebook url is invalid')
-    assert page.has_content?('Contact email is invalid')
   end
 end
