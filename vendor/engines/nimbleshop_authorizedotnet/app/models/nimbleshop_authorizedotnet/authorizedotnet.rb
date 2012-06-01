b9d31eecd329dd789b12e1bf117300b9327dc7a5
Neeraj Singh <neerajdotname@gmail.com>
2012-06-01 10:36:25 -0400

    bring ssl enable or disable feature to ui

--- vendor/engines/nimbleshop_authorizedotnet/app/models/nimbleshop_authorizedotnet/authorizedotnet.rb
+++ vendor/engines/nimbleshop_authorizedotnet/app/models/nimbleshop_authorizedotnet/authorizedotnet.rb
@@ -1,9 +1,9 @@
 module NimbleshopAuthorizedotnet
   class Authorizedotnet < PaymentMethod
 
-    store_accessor :metadata, :login_id, :transaction_key, :company_name_on_creditcard_statement, :mode
+    store_accessor :metadata, :login_id, :transaction_key, :company_name_on_creditcard_statement, :mode, :ssl
 
-    before_save :set_mode
+    before_save :set_mode, :set_ssl
 
     validates_presence_of :login_id, :transaction_key, :company_name_on_creditcard_statement
 
@@ -15,6 +15,10 @@ module NimbleshopAuthorizedotnet
 
     def set_mode
       self.mode ||= 'test'
+    end
+
+    def set_ssl
+      self.ssl ||= 'disabled'
     end
 
   end
