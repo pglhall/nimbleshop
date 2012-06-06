8843b79189a337c07da8a5aefe7f8f33515a6b3c
Neeraj Singh <neerajdotname@gmail.com>
2012-06-06 15:08:08 -0400

    use post_params while updating shop

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -7,11 +7,27 @@ class Admin::ShopsController < AdminController
   end
 
   def update
-    if current_shop.update_attributes(params[:shop])
+    if current_shop.update_attributes(post_params[:shop])
       redirect_to edit_admin_shop_path, notice: 'Shop was successfully updated'
     else
       respond_with(:admin, current_shop)
     end
+  end
+
+  private
+
+  def post_params
+    params.permit(shop: [ :name,
+                          :theme,
+                          :author_name,
+                          :description,
+                          :time_zone,
+                          :from_email,
+                          :default_creditcard_action,
+                          :twitter_handle,
+                          :facebook_url,
+                          :google_analytics_tracking_id,
+                          :tax_percentage])
   end
 
 end
