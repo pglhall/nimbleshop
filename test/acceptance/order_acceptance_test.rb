96fa22f14fa3902d90cbb602e0cc340cbda51309
Neeraj Singh <neerajdotname@gmail.com>
2012-06-06 10:23:25 -0400

    no need to play casette here

--- test/acceptance/order_acceptance_test.rb
+++ test/acceptance/order_acceptance_test.rb
@@ -3,14 +3,10 @@ require 'test_helper'
 class OrderAcceptanceTest < ActionDispatch::IntegrationTest
 
   setup do
-    @order = create(:order)
-    processor = NimbleshopAuthorizedotnet::Processor.new(@order)
-    playcasette('authorize.net/authorize-success') do
-      processor.authorize(creditcard: build(:creditcard))
-    end
+    @order = create :order_paid_using_authorizedotnet
   end
 
-  test "show order" do
+  test "show order when it was paid using authorizedotnet" do
     visit order_path(@order)
     assert page.has_content?('Purchase is complete')
     assert page.has_content?('In the credit card statement name of the company would appear as Nimbleshop LLC')
