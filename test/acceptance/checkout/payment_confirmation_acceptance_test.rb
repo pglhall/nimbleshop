07df0e404254cc1f5a023bd744d45a5b43632701
Neeraj Singh <neerajdotname@gmail.com>
2012-06-06 10:27:22 -0400

    payment confirmation test for paypalwp and splitable

--- test/acceptance/checkout/payment_confirmation_acceptance_test.rb
+++ test/acceptance/checkout/payment_confirmation_acceptance_test.rb
@@ -2,12 +2,21 @@ require 'test_helper'
 
 class PaymentConfirmationAcceptanceTest < ActionDispatch::IntegrationTest
 
-  setup do
-    @order = create :order_paid_using_authorizedotnet
+  test "show order when it was paid using splitable" do
+    order = create :order_paid_using_splitable
+    visit order_path(order)
+    assert page.has_content?('Purchase is complete')
+  end
+
+  test "show order when it was paid using paypalwp" do
+    order = create :order_paid_using_paypalwp
+    visit order_path(order)
+    assert page.has_content?('Purchase is complete')
   end
 
   test "show order when it was paid using authorizedotnet" do
-    visit order_path(@order)
+    order = create :order_paid_using_authorizedotnet
+    visit order_path(order)
     assert page.has_content?('Purchase is complete')
     assert page.has_content?('In the credit card statement name of the company would appear as Nimbleshop LLC')
   end
