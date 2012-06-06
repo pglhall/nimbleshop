4d39edbdf44f4a53242ffa433f1ddccfe17b1d16
Neeraj Singh <neerajdotname@gmail.com>
2012-06-06 06:07:11 -0400

    payment_transaction_factory with all data

--- test/factories/payment_transaction_factory.rb
+++ test/factories/payment_transaction_factory.rb
@@ -8,6 +8,17 @@ FactoryGirl.define do
       operation 'authorized'
       amount 11200
       transaction_gid "2172281042"
+      params do
+        { response_code: 1,
+          response_reason_code: "1",
+          response_reason_text: "This transaction has been approved.",
+          avs_result_code: "Y",
+          transaction_id: "2172281042",
+          card_code: "P" }
+      end
+      metadata do
+        { card_number: 'XXXX-XXXX-XXXX-0027' }
+      end
     end
 
     factory :payment_transaction_with_authorizedotnet,   traits: [ :authorizedotnet ]
