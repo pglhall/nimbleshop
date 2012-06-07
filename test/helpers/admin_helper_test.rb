b81be0387836479da8afb3afb546eaeadad9b73c
Neeraj Singh <neerajdotname@gmail.com>
2012-06-07 15:26:28 -0400

    fixing test

--- test/helpers/admin_helper_test.rb
+++ test/helpers/admin_helper_test.rb
@@ -9,29 +9,29 @@ class AdminHelperTest < ActiveRecord::TestCase
   end
 
   test "admin_products_path" do
-    assert helper.active?('admin_products_path', 'admin/products')
-    refute helper.active?('admin_products_path', 'admin/orders')
+    assert helper.admin_topbar_active?('admin_products_path', 'admin/products')
+    refute helper.admin_topbar_active?('admin_products_path', 'admin/orders')
 
-    assert helper.active?('admin_custom_fields_path', 'admin/custom_fields')
-    refute helper.active?('admin_custom_fields_path', 'admin/orders')
+    assert helper.admin_topbar_active?('admin_custom_fields_path', 'admin/custom_fields')
+    refute helper.admin_topbar_active?('admin_custom_fields_path', 'admin/orders')
 
-    assert helper.active?('admin_product_groups_path', 'admin/product_groups')
-    refute helper.active?('admin_product_groups_path', 'admin/orders')
+    assert helper.admin_topbar_active?('admin_product_groups_path', 'admin/product_groups')
+    refute helper.admin_topbar_active?('admin_product_groups_path', 'admin/orders')
 
-    assert helper.active?('admin_link_groups_path', 'admin/link_groups')
-    refute helper.active?('admin_link_groups_path', 'admin/orders')
+    assert helper.admin_topbar_active?('admin_link_groups_path', 'admin/link_groups')
+    refute helper.admin_topbar_active?('admin_link_groups_path', 'admin/orders')
 
-    assert helper.active?('admin_shipping_zones_path', 'admin/shipping_zones')
-    assert helper.active?('admin_shipping_zones_path', 'admin/shipping_methods')
-    refute helper.active?('admin_shipping_zones_path', 'admin/orders')
+    assert helper.admin_topbar_active?('admin_shipping_zones_path', 'admin/shipping_zones')
+    assert helper.admin_topbar_active?('admin_shipping_zones_path', 'admin/shipping_methods')
+    refute helper.admin_topbar_active?('admin_shipping_zones_path', 'admin/orders')
 
-    assert helper.active?('admin_payment_methods_path', 'admin/payment_methods')
-    refute helper.active?('admin_payment_methods_path', 'admin/orders')
+    assert helper.admin_topbar_active?('admin_payment_methods_path', 'admin/payment_methods')
+    refute helper.admin_topbar_active?('admin_payment_methods_path', 'admin/orders')
 
-    assert helper.active?('edit_admin_shop_path', 'admin/shops')
-    refute helper.active?('edit_admin_shop_path', 'admin/orders')
+    assert helper.admin_topbar_active?('edit_admin_shop_path', 'admin/shops')
+    refute helper.admin_topbar_active?('edit_admin_shop_path', 'admin/orders')
 
-    assert helper.active?('admin_orders_path', 'admin/orders')
+    assert helper.admin_topbar_active?('admin_orders_path', 'admin/orders')
   end
 
 end
