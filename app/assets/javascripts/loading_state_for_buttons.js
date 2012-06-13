fd1d9bcc80aa29699db21c3dc4ef3ada075cda60
Neeraj Singh <neerajdotname@gmail.com>
2012-06-13 15:11:37 -0400

    fix processing message

--- app/assets/javascripts/loading_state_for_buttons.js
+++ app/assets/javascripts/loading_state_for_buttons.js
@@ -1,7 +1,8 @@
 $(function(){
-  $('.btn').click(function () {
+  $("[data-loading-text]").click(function () {
     var btn = $(this);
     btn.button('loading');
     //setTimeout(function () { btn.button('reset'); }, 3000);
   });
 })
+
