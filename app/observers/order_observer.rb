02da86da16ea5a63e56a5c75daa3f3768ba5fb3f
Neeraj Singh <neerajdotname@gmail.com>
2012-06-20 17:02:34 -0400

    getting rid of generic message

--- app/observers/order_observer.rb
+++ app/observers/order_observer.rb
@@ -1,16 +1,17 @@
 class OrderObserver < ActiveRecord::Observer
 
-  # TODO send_email_notification is a very generic name
   def after_purchase(order, transition)
-    send_email_notifications(order)
+    Mailer.delay.order_notification_to_buyer(order.number)
+    AdminMailer.delay.new_order_notification(order.number)
+
     order.mark_as_purchased!
     order.shipping_pending
     ActiveSupport::Notifications.instrument("order.purchased", { order_number: order.number } )
   end
 
-  # TODO send_email_notification is a very generic name
   def after_authorize(order, transition)
-    send_email_notifications(order)
+    Mailer.delay.order_notification_to_buyer(order.number)
+    AdminMailer.delay.new_order_notification(order.number)
     order.mark_as_purchased!
     order.shipping_pending
   end
@@ -22,15 +23,6 @@ class OrderObserver < ActiveRecord::Observer
   end
 
   def after_refund(order, transition)
-  end
-
-  private
-
-  # TODO  If a person pays using credit card then that person will receive the order
-  # notification twice. 
-  def send_email_notifications(order)
-    Mailer.delay.order_notification_to_buyer(order.number)
-    AdminMailer.delay.new_order_notification(order.number)
   end
 
 end
