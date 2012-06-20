c4d59e7742d258b3695a2abc72d47faa9260c159
Neeraj Singh <neerajdotname@gmail.com>
2012-06-20 17:25:10 -0400

    updated order observer with transition comments

--- app/observers/order_observer.rb
+++ app/observers/order_observer.rb
@@ -1,5 +1,20 @@
 class OrderObserver < ActiveRecord::Observer
 
+  # All the methods listed below get transition object as the second
+  # parameter. It is an instance of +StateMachine::Transition+.
+  #
+  # p transition.to => 'pending'
+  # p transition.from => 'abandoned'
+  # p transition.event => :pending
+  # p transition => #<StateMachine::Transition attribute=:payment_status event=:pending from="abandoned" from_name=:abandoned to="pending" to_name=:pending>
+  #
+
+  #
+  # Ideally what to do after state transition should be pushed to
+  # individual payment method. In this way we will not have the if
+  # condition that we are seeing in after_pending .
+  #
+
   def after_purchase(order, transition)
     Mailer.delay.order_notification_to_buyer(order.number)
     AdminMailer.delay.new_order_notification(order.number)
