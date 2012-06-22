a0fda23672683dcee892ecc79d66e7d3c599987d
Neeraj Singh <neerajdotname@gmail.com>
2012-06-22 11:36:09 -0400

    removed not useful initializer

--- config/initializers/field_with_error.rb
+++
@@ -1,7 +0,0 @@
-ActionView::Base.field_error_proc = Proc.new { |html_tag, instance|
-  html_tag.html_safe
-}
-
-#ActionView::Base.field_error_proc = Proc.new { |html_tag, instance|
-  #%(<span class="fieldWithErrors2">#{html_tag}</span>).html_safe
-#}
