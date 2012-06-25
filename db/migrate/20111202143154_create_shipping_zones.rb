aea2910e6b611ae7639d55f2d86fe5cc381ce138
Neeraj Singh <neerajdotname@gmail.com>
2012-06-24 22:49:47 -0400

    removed TODO

--- db/migrate/20111202143154_create_shipping_zones.rb
+++ db/migrate/20111202143154_create_shipping_zones.rb
@@ -1,18 +1,17 @@
 class CreateShippingZones < ActiveRecord::Migration
+
   def change
     create_table :shipping_zones do |t|
       t.string  :name
       t.string  :permalink,     null: false
       t.string  :country_code
       t.string  :state_code
-
-      # TODO megpha it should be not nullable. It is a hack being added
-      # by Neeraj because migration to rails 3.2 is failing
       t.string  :type,          null: true
-
       t.integer :country_shipping_zone_id
       t.timestamps
     end
+
     add_index :shipping_zones, :permalink, unique: true
   end
+
 end
