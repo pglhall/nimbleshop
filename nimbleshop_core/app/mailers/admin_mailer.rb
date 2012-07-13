f774c79a09b21c61e0c28a1245625e03ec15018b
Neeraj Singh <neerajdotname@gmail.com>
2012-07-13 17:42:12 -0400

    use bare_url_with_port

--- nimbleshop_core/app/mailers/admin_mailer.rb
+++ nimbleshop_core/app/mailers/admin_mailer.rb
@@ -4,7 +4,7 @@ class AdminMailer < ActionMailer::Base
 
   default from: lambda { Shop.first.from_email }
 
-  default_url_options[:host]     = Nimbleshop.config.host_for_email
+  default_url_options[:host]     = Nimbleshop.config.bare_url_with_port
   default_url_options[:protocol] = 'http'
 
   def new_order_notification(order_number)
