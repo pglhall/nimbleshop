aaf9d51aa3a875d70e856b262e5bdd6fe973ed38
Neeraj Singh <neerajdotname@gmail.com>
2012-07-14 22:22:50 -0400

    be consistent and keep everything simply

--- nimbleshop_simply/lib/nimbleshop_simply/engine.rb
+++ nimbleshop_simply/lib/nimbleshop_simply/engine.rb
@@ -5,6 +5,7 @@ module NimbleshopSimply
 
     initializer 'nimbleshop_simply.precompile_assets' do |config|
       Rails.application.config.assets.precompile += %w( nimbleshop_simply/simply.css )
+      Rails.application.config.assets.precompile += %w( nimbleshop_simply/simply.js )
     end
 
   end
