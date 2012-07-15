cc2dadc793e641fa589f309c7a33514286a312be
Neeraj Singh <neerajdotname@gmail.com>
2012-07-15 15:43:07 -0400

    adding order directory

---
+++ nimbleshop_core/app/models/order/payment_transitions.rb
@@ -0,0 +1,56 @@
+module Order::PaymentTransitions
+
+  # All the methods listed below get transition object as the second
+  # parameter. It is an instance of +StateMachine::Transition+.
+  #
+  # p transition.to => 'pending'
+  # p transition.from => 'abandoned'
+  # p transition.event => :pending
+  # p transition => #<StateMachine::Transition attribute=:payment_status event=:pending from="abandoned" from_name=:abandoned to="pending" to_name=:pending>
+  #
+
+  #
+  # Ideally what to do after state transition should be pushed to
+  # individual payment method. In this way we will not have the if
+  # condition that we are seeing in after_pending .
+  #
+
+  def after_purchase(order, transition)
+    mailer = Nimbleshop.config.mailer.constantize
+    mailer.delay.order_notification_to_buyer(order.number)
+    AdminMailer.delay.new_order_notification(order.number)
+
+    order.mark_as_purchased!
+    order.shipping_pending
+    ActiveSupport::Notifications.instrument("order.purchased", { order_number: order.number } )
+  end
+
+  def after_authorize(order, transition)
+    mailer = Nimbleshop.config.mailer.constantize
+    mailer.delay.order_notification_to_buyer(order.number)
+    AdminMailer.delay.new_order_notification(order.number)
+
+    order.mark_as_purchased!
+    order.shipping_pending
+  end
+
+  def after_kapture(order, transition)
+  end
+
+  def after_pending(order, transition)
+    if order.payment_method.is_a? NimbleshopCod::Cod
+      mailer = Nimbleshop.config.mailer.constantize
+      mailer.order_notification_to_buyer(order.number).deliver
+      AdminMailer.delay.new_order_notification(order.number)
+    end
+
+    order.shipping_pending
+  end
+
+  def after_void(order, transition)
+  end
+
+  def after_refund(order, transition)
+  end
+
+end
