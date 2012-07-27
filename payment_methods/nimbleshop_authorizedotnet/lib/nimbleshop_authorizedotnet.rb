3e1b55784abc2d05e51d28388139b347464c4314
Neeraj Singh <neerajdotname@gmail.com>
2012-07-26 22:03:59 -0400

    moved util out to its own file

--- payment_methods/nimbleshop_authorizedotnet/lib/nimbleshop_authorizedotnet.rb
+++ payment_methods/nimbleshop_authorizedotnet/lib/nimbleshop_authorizedotnet.rb
@@ -1,6 +1,7 @@
-require "nimbleshop_authorizedotnet/engine"
+require 'nimbleshop_authorizedotnet/engine'
 
 module NimbleshopAuthorizedotnet
-  autoload :Processor, "nimbleshop_authorizedotnet/processor"
-  autoload :Gateway,   "nimbleshop_authorizedotnet/gateway"
+  autoload :Processor, 'nimbleshop_authorizedotnet/processor'
+  autoload :Gateway,   'nimbleshop_authorizedotnet/gateway'
+  autoload :Util,      'nimbleshop_authorizedotnet/util'
 end
