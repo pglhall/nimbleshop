9d2b9ceb3f0fb1c3bf73de7f695076b2be25dce6
Neeraj Singh <neerajdotname@gmail.com>
2012-08-01 18:23:20 -0400

    changed variable name from record to payment_method

--- payment_methods/nimbleshop_authorizedotnet/lib/nimbleshop_authorizedotnet/gateway.rb
+++ payment_methods/nimbleshop_authorizedotnet/lib/nimbleshop_authorizedotnet/gateway.rb
@@ -1,11 +1,11 @@
 module NimbleshopAuthorizedotnet
   module Gateway
     def self.instance
-      record = NimbleshopAuthorizedotnet::Authorizedotnet.first
+      payment_method = NimbleshopAuthorizedotnet::Authorizedotnet.first
 
-      ActiveMerchant::Billing::Gateway.logger = Rails.logger if record.mode.to_s == 'test'
+      ActiveMerchant::Billing::Gateway.logger = Rails.logger if payment_method.mode.to_s == 'test'
 
-      ActiveMerchant::Billing::AuthorizeNetGateway.new( record.credentials )
+      ActiveMerchant::Billing::AuthorizeNetGateway.new( payment_method.credentials )
     end
   end
 end
